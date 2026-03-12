fe3498e1547a89c956896c06cc2b2c9204495f41
muneeb1st <muneeburehman1st@gmail.com>
2026-03-12 05:32:56 +0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Research Vault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -17,6 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "muneeb1st.github.io/quartz",
+    useIndexHtml: true, // <--- ADD THIS LINE
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
