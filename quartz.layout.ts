4f9c59cf49daeb038a24bd2eb226956f859f63d9
muneeb1st <muneeburehman1st@gmail.com>
2026-03-18 16:43:09 +0400

    ⚙️ Update feature toggles via Admin Dashboard

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,7 +18,7 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
-    // component: Component.Breadcrumbs(),
+    component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
